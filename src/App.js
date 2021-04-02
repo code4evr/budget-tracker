84c0d049d4deda99b003219540898fc1a446c663
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-04-03 00:00:32 +0530

    modified to incorporate the new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import BudgetContainer from './Components/BudgetContainer';
+import ExpenseList from './Components/ExpenseList';
+import Navbar from './Components/Navbar';
+import AddExpenseForm from './Components/AddExpenseForm';
+import Sidebar from './Components/Sidebar';
+import { AppContext, AppProvider } from './Context/AppContext';
+import './main.css';
 
 const App = () => {
   return (
-    <div>
-      <h1>Budget Tracker</h1>
-    </div>
+    <AppProvider>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-2 bg-light">
+            <Sidebar />
+          </div>
+          <div className="col-sm-10">
+            <Navbar />
+            <h1 className="mt-3">Budget Planner</h1>
+            <div className="mt-3 row">
+              <BudgetContainer />
+            </div>
+            <h1 className="mt-3">Expenses</h1>
+            <div className="row mt-3">
+              <div className="col-sm">
+                <input
+                  className="col-sm-12 form-control"
+                  type="search"
+                  name="item-search"
+                  id="item-search"
+                  placeholder="search"
+                />
+              </div>
+              <div className="col-sm-12 mt-3 mb-3">
+                <ExpenseList />
+              </div>
+              <div className="col-sm-12">
+                <AddExpenseForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </AppProvider>
   );
 };
 
