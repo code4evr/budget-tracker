b2ad770471cf6844ebab4e528aa6994c1862db2e
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-04-04 23:39:24 +0530

    sidebar component for the dashboard

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react';
+import { BsFillHouseFill } from 'react-icons/bs';
+import {
+  FaReceipt,
+  FaChartPie,
+  FaFileInvoiceDollar,
+} from 'react-icons/fa';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar-container bg-light">
+      <div className="sidebar-brand">
+        <h1>Dashboard</h1>
+      </div>
+      <ul className="sidebar-list mt-4">
+        <li className="sidebar-item d-flex">
+          <span>
+            <BsFillHouseFill />
+          </span>
+          <span className="pl-2">Home</span>
+        </li>
+        <li className="sidebar-item d-flex">
+          <span>
+            <FaFileInvoiceDollar />
+          </span>
+          <span className="pl-2">New Budget</span>
+        </li>
+        <li className="sidebar-item d-flex">
+          <span>
+            <FaReceipt />
+          </span>
+          <span className="pl-2">Add Expense</span>
+        </li>
+        <li className="sidebar-item d-flex">
+          <span>
+            <FaChartPie />
+          </span>
+          <span className="pl-2">Analytics</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
