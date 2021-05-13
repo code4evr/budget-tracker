fe76d73722a4324b30ef0d96bc2a2a9f98c7a71b
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-05-13 23:36:56 +0530

    budget tracker landing page

---
+++ src/Components/Homepage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Landing</div>
+    </>
+  );
+};
+
+export default HomePage;
