9e0bd7c7311f59f63196401aedf60bfbf79dc007
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-07-16 22:49:35 +0530

    css classes for sidebar changed and a new div sidebar-overlay added

--- src/App.js
+++ src/App.js
@@ -1,48 +1,93 @@
-import React from 'react';
-import BudgetContainer from './Components/BudgetContainer';
-import ExpenseList from './Components/ExpenseList';
-import Navbar from './Components/Navbar';
-import AddExpenseForm from './Components/AddExpenseForm';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import BudgetComponent from './Components/Expenses/BudgetComponent';
+import Navbar from './Components/Navbar/Navbar';
 import Sidebar from './Components/Sidebar';
-import { AppContext, AppProvider } from './Context/AppContext';
+import Home from './Components/BudgetHome/Home';
+import HomePage from './Components/Homepage/HomePage';
+import Login from './Components/User/Login';
+import Register from './Components/User/Registration';
+import { AppProvider } from './Context/AppContext';
+import { BudgetProvider } from './Context/BudgetContext';
+import { AuthProvider } from './Context/AuthContext';
+import AuthRoute from './utils/AuthRoute';
+import SettingsPage from './Components/Settings/SettingsPage';
+import Analytics from './Components/Analytics/Analytics';
 import './main.css';
 
 const App = () => {
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!toggle);
+    console.log(toggle);
+  };
   return (
-    <AppProvider>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-sm-2 bg-light">
-            <Sidebar />
-          </div>
-          <div className="col-sm-10">
-            <Navbar />
-            <h1 className="mt-3">Budget Planner</h1>
-            <div className="mt-3 row">
-              <BudgetContainer />
-            </div>
-            <h1 className="mt-3">Expenses</h1>
-            <div className="row mt-3">
-              <div className="col-sm">
-                <input
-                  className="col-sm-12 form-control"
-                  type="search"
-                  name="item-search"
-                  id="item-search"
-                  placeholder="search"
-                />
-              </div>
-              <div className="col-sm-12 mt-3 mb-3">
-                <ExpenseList />
-              </div>
-              <div className="col-sm-12">
-                <AddExpenseForm />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </AppProvider>
+    <AuthProvider>
+      <Router>
+        <Route exact path="/" component={HomePage} />
+        <Switch>
+          <Route path="/app">
+            <AppProvider>
+              <BudgetProvider>
+                <div className="wrapper">
+                  <div
+                    className={
+                      toggle
+                        ? 'sidebar-overlay'
+                        : 'sidebar-overlay show'
+                    }
+                  >
+                    <div
+                      className={
+                        toggle ? 'sidebar-hide' : 'sidebar-show'
+                      }
+                    >
+                      <Sidebar val={toggle} />
+                    </div>
+                  </div>
+                  <div
+                    className={
+                      toggle
+                        ? 'main-container main-container-grow'
+                        : 'main-container'
+                    }
+                  >
+                    <Navbar onclick={handleToggle} />
+                    <div className="content">
+                      <div className="container-fluid">
+                        <Switch>
+                          <Route path="/app/home">
+                            <Home />
+                          </Route>
+                          <Route path="/app/analytics">
+                            <Analytics />
+                          </Route>
+                          <Route path="/app/budget/:id">
+                            <div>
+                              <BudgetComponent />
+                            </div>
+                          </Route>
+                        </Switch>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </BudgetProvider>
+            </AppProvider>
+          </Route>
+          <Route path="/prefs/settings">
+            <SettingsPage />
+          </Route>
+        </Switch>
+        <AuthRoute path="/login" component={Login} />
+        <AuthRoute path="/register" component={Register} />
+      </Router>
+    </AuthProvider>
   );
 };
 
