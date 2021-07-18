44fad4b921000a5fe5379514f3a31efd249dba58
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-07-18 22:16:41 +0530

    new budget modal added

--- src/Components/NewBudget.js
+++ src/Components/NewBudget.js
@@ -1,54 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Button from './Buttons/Buttons';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleModal } from '../Redux/toggleModalSlice';
+import { FaTimes } from 'react-icons/fa';
+import './newBudget.css';
 
 const NewBudget = () => {
-  return (
-    <div>
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Launch demo modal
-      </button>
+  const dispatch = useDispatch();
+  const toggleVal = useSelector(state => state.modal);
+  console.log(toggleVal);
 
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Modal title
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
+  const handleClick = () => {
+    dispatch(
+      toggleModal({
+        val: !toggleVal.toggle,
+      }),
+    );
+
+    document.body.style.position = '';
+  };
+
+  return (
+    <>
+      {toggleVal.toggle ? (
+        <div className="modal-overlay">
+          <div className="__modal">
+            <div className="modal__header">
+              <span>New Budget</span>
             </div>
-            <div className="modal-body">...</div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              <button type="button" className="btn btn-primary">
-                Save changes
-              </button>
+            <div className="modal__body">
+              <span>Hello</span>
+            </div>
+            <div className="modal__footer">
+              <Button
+                onclick={handleClick}
+                classname="close"
+                buttonName="Close"
+                faTimes={<FaTimes />}
+              />
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        ''
+      )}
+    </>
   );
 };
 
