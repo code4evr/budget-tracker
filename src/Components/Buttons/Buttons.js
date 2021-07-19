e0aaf7fe63fd5a0953cd7fb80185dfba1d4b1922
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-07-19 22:51:07 +0530

    button component modified

--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -1,38 +1,13 @@
 import React from 'react';
-import { FaArrowLeft } from 'react-icons/fa';
 
-const CancelButton = () => {
+const Button = ({ onclick, classname, buttonName, faTimes }) => {
   return (
     <div>
-      <button className="btn btn-cancel">Cancel</button>
-    </div>
-  );
-};
-
-const UpdateButton = () => {
-  return (
-    <div>
-      <button className="btn btn-upadate">Update</button>
-    </div>
-  );
-};
-
-const BackButton = () => {
-  return (
-    <div>
-      <button className="btn btn-back">Back</button>
-    </div>
-  );
-};
-
-const BackButtonArrow = () => {
-  return (
-    <div>
-      <button className="btn btn-arrow">
-        <FaArrowLeft />
+      <button className={classname} onClick={() => onclick()}>
+        {faTimes || buttonName}
       </button>
     </div>
   );
 };
 
-export { CancelButton, UpdateButton, BackButton, BackButtonArrow };
+export default Button;
