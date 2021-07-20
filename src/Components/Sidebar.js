43dffc85f518108efc87bb8bd5e9eb00fb327656
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-07-20 22:42:42 +0530

    add expense button linked with the new expense modal. handleClick function modified

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Link } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  toggleBudgetModal,
+  toggleExpenseModal,
+} from '../Redux/toggleModalSlice';
+import { Link } from 'react-router-dom';
 import { BsFillHouseFill } from 'react-icons/bs';
 import {
   FaReceipt,
@@ -8,44 +13,66 @@ import {
 } from 'react-icons/fa';
 
 const Sidebar = props => {
+  const dispatch = useDispatch();
+  const toggleVal = useSelector(state => state.modal);
+  console.log(toggleVal);
+  const handleClick = handler => e => {
+    if (handler === 'newBudget') {
+      dispatch(
+        toggleBudgetModal({
+          val: !toggleVal.toggleBudget,
+        }),
+      );
+
+      document.body.style.position = 'fixed';
+    } else if (handler === 'newExpense') {
+      dispatch(
+        toggleExpenseModal({
+          val: !toggleVal.toggleExpense,
+        }),
+      );
+
+      document.body.style.position = 'fixed';
+    }
+  };
   return (
-    <div
-      className={
-        props.val
-          ? 'sidebar-container bg-light hide-sidebar'
-          : 'sidebar-container bg-light'
-      }
-    >
+    <>
       <div className="sidebar-brand">
-        <h1>Dashboard</h1>
+        <p>Budgetree</p>
       </div>
-      <ul className="sidebar-list mt-4">
-        <li className="sidebar-item d-flex">
+      <ul className="sidebar-menu">
+        <Link className="sidebar-item" to="/app/home">
           <span>
             <BsFillHouseFill />
           </span>
-          <span className="pl-2">Home</span>
-        </li>
-        <li className="sidebar-item d-flex">
+          <span>Home</span>
+        </Link>
+        <Link
+          className="sidebar-item"
+          onClick={handleClick('newBudget')}
+        >
           <span>
             <FaFileInvoiceDollar />
           </span>
-          <span className="pl-2">New Budget</span>
-        </li>
-        <li className="sidebar-item d-flex">
+          <span>New Budget</span>
+        </Link>
+        <Link
+          className="sidebar-item"
+          onClick={handleClick('newExpense')}
+        >
           <span>
             <FaReceipt />
           </span>
-          <span className="pl-2">Add Expense</span>
-        </li>
-        <li className="sidebar-item d-flex">
+          <span>Add Expense</span>
+        </Link>
+        <Link to="/app/analytics" className="sidebar-item">
           <span>
             <FaChartPie />
           </span>
-          <span className="pl-2">Analytics</span>
-        </li>
+          <span>Analytics</span>
+        </Link>
       </ul>
-    </div>
+    </>
   );
 };
 
