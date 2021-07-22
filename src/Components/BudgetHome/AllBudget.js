ccdcc4987d9a96ca113496c1a37a7e3a02f3ca3d
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-07-22 22:56:43 +0530

    name changed from home budget to allBudget

---
+++ src/Components/BudgetHome/AllBudget.js
@@ -0,0 +1,123 @@
+import React, { useState, useContext, useEffect } from 'react';
+import AddBudgetImg from '../../assets/undraw_new_entries_nh3h.svg';
+import { BsPlusCircleFill } from 'react-icons/bs';
+import BudgetList from './BudgetList';
+import BudgetForm from './BudgetForm';
+import { GET_BUDGETS } from '../../Queries/Queries';
+import { today } from '../../assets/date';
+import { useHistory, Redirect } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { AuthContext } from '../../Context/AuthContext';
+import './budgethome.css';
+
+const AllBudget = props => {
+  const history = useHistory();
+  // get auth from AuthContext
+  const { user, timeFrame } = useContext(AuthContext);
+  console.log(user);
+
+  // boolean for toggling budget form
+  const [bool, setBool] = useState(false);
+
+  const result = useQuery(GET_BUDGETS);
+
+  const handleClick = handler => e => {
+    if (handler === 'new-budget') {
+      setBool(!bool);
+    } else if (handler === 'cancel') {
+      setBool(!bool);
+    } else if (handler === 'save') {
+      console.log('create button clicked');
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      console.log('inside use effect');
+      setTimeout(() => {
+        window.location.reload();
+        console.log('reloaded');
+      }, timeFrame);
+    }
+  }, [user]);
+
+  if (result.loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      {user ? (
+        <>
+          {result.data.getBudgets.length > 0 ? (
+            <>
+              {result.data.getBudgets.map(b => (
+                <BudgetList
+                  key={b.id}
+                  budgetId={b.id}
+                  budgetname={b.title}
+                />
+              ))}
+              {bool ? (
+                <BudgetForm onclick={handleClick} />
+              ) : (
+                <div className="row">
+                  <div
+                    className="create-budget col-sm-12"
+                    onClick={handleClick('new-budget')}
+                  >
+                    <span>
+                      <BsPlusCircleFill />
+                    </span>
+                    <span className="pl-2 pt-1">
+                      Create New Budget
+                    </span>
+                  </div>
+                </div>
+              )}
+            </>
+          ) : (
+            <div className="row mt-3">
+              <div className="col-sm-12">
+                <div className="today">
+                  <span>Today</span>{' '}
+                  <span className="date-today">{today}</span>
+                </div>
+                {!bool ? (
+                  <div
+                    className="mt-2 d-flex align-items-center create-budget"
+                    onClick={handleClick('new-budget')}
+                  >
+                    <span>
+                      <BsPlusCircleFill />
+                    </span>
+                    <span className="pl-2 pt-1">
+                      Create New Budget
+                    </span>
+                  </div>
+                ) : (
+                  ''
+                )}
+                {bool ? (
+                  <BudgetForm onclick={handleClick} />
+                ) : (
+                  <div className="img-container d-flex justify-content-center align-items-center">
+                    <img
+                      src={AddBudgetImg}
+                      alt="add-budget"
+                      className="add-budget"
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
+        </>
+      ) : (
+        <Redirect to="/login" />
+      )}
+    </>
+  );
+};
+
+export default AllBudget;
