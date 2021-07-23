84760550cec33ec7f36818d6264171e1e7d9e58d
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-07-23 22:50:37 +0530

    clickCheckbox reducer added

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import toggleModalSlice from './toggleModalSlice';
+import toggleUtilityButton from './toggleUtilityButton';
 
 export default configureStore({
   reducer: {
     modal: toggleModalSlice,
+    utility: toggleUtilityButton,
   },
 });
