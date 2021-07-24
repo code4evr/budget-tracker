0a0f2c7987e4acfbf2cd0e9a748a6d92cf80dfa7
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-07-24 21:58:06 +0530

    new buttons added for delete, archive and add user, handleinputchange function declared, hooks related to redux added

--- src/Components/UtilityBar/UtilityBar.js
+++ src/Components/UtilityBar/UtilityBar.js
@@ -1,45 +1,58 @@
 import React from 'react';
+import { clickCheckbox } from '../../Redux/toggleUtilityButton';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   FaTrash,
   FaAngleLeft,
   FaAngleRight,
   FaPlusSquare,
+  FaArchive,
 } from 'react-icons/fa';
 import './utilityBar.css';
 
 const UtilityBar = () => {
+  const dispatch = useDispatch();
+  const toggleVal = useSelector(state => state.utility);
+  console.log(toggleVal);
+  const handleInputChange = e => {
+    dispatch(
+      clickCheckbox({
+        val: !toggleVal.toggleUtility,
+      }),
+    );
+  };
   return (
-    <div className="utility-bar">
-      <div className="row">
-        <div className="utility-container">
-          <div className="utility">
-            <div className="utility-checkbox">
-              <input
-                type="checkbox"
-                name="check-budget"
-                className="form-control"
-              />
+    <>
+      <div className="row utility-bar">
+        <div className="checkbox-container">
+          <input
+            type="checkbox"
+            name="checkbox"
+            className="checkbox"
+          />
+          <div className="other-utility">
+            <div className="utility-btn">
+              <FaTrash size="1.3rem" />
             </div>
-            <div className="other-utilities">
-              <div className="delete">
-                <FaTrash />
-              </div>
-              <div className="add">
-                <FaPlusSquare />
-              </div>
+            <div className="utility-btn">
+              <FaPlusSquare size="1.4rem" />
             </div>
-          </div>
-          <div className="page-control">
-            <div className="page-control-left">
-              <FaAngleLeft />
-            </div>
-            <div className="page-control-right">
-              <FaAngleRight />
+            <div className="utility-btn">
+              <FaArchive size="1.4rem" />
             </div>
           </div>
         </div>
+
+        <div className="page-control">
+          <div className="page-left">
+            <FaAngleLeft />
+          </div>
+          <div className="page-right">
+            <FaAngleRight />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
