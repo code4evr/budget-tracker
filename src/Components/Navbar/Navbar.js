c90aaa0c7652a247bc3a48bf38afa421bdb57e39
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2021-08-01 20:57:01 +0530

    dashboard navbar toggle button and settings button added. function for handling click added.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,53 +1,65 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleBudgetModal } from '../../Redux/toggleModalSlice';
 import { BsGearFill, BsPlusCircle } from 'react-icons/bs';
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaPlus, FaCog } from 'react-icons/fa';
 import Settings from '../Settings/Settings';
 import { useOutsideClick } from '../../utils/customHooks';
 import './navbar.css';
 
 const Navbar = props => {
+  const dispatch = useDispatch();
+  const toggleVal = useSelector(state => state.modal);
   const [toggle, setToggle] = useState(false);
 
   const clickRef = useOutsideClick(() => {
     setToggle(!toggle);
   });
 
-  const handleClick = () => {
-    setToggle(!toggle);
+  const handleClick = handler => e => {
+    if (handler === 'settings') {
+      setToggle(!toggle);
+    } else if (handler === 'newBudget') {
+      dispatch(
+        toggleBudgetModal({
+          val: !toggleVal.toggleBudget,
+        }),
+      );
+
+      document.body.style.position = 'fixed';
+    }
   };
 
   console.log(toggle);
 
   return (
     <>
-      <div
-        className={
-          props.val
-            ? 'nav-container nav-container-grow'
-            : 'nav-container'
-        }
-      >
-        <nav className="navbar navbar-expand-lg navbar-light bg-light mt-2 py-3">
-          <div className="container-fluid pr-0">
-            <ul className="navbar-nav mr-auto mb mb-lg-0 flex-row">
-              <li className="nav-item">
-                <FaBars
-                  size="1.65rem"
-                  onClick={() => props.onclick()}
-                />
-              </li>
-            </ul>
-            <ul className="navbar-nav ml-auto mb mb-lg-0 flex-row">
-              <li className="nav-item pr-2">
-                <BsPlusCircle size="1.65rem" />
-              </li>
-              <li className="nav-item pl-3" onClick={handleClick}>
-                <BsGearFill size="1.65rem" />
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
+      <nav className={props.val ? 'navbar navbar-toggle' : 'navbar'}>
+        <div className="container-fluid">
+          <ul className="navbar-nav mr-auto mb mb-lg-0 flex-row">
+            <li className="nav-item">
+              <FaBars
+                size="1.15rem"
+                onClick={() => props.onclick()}
+              />
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto mb mb-lg-0 flex-row">
+            <li
+              className="nav-item pr-2"
+              onClick={handleClick('newBudget')}
+            >
+              <FaPlus size="1.35rem" />
+            </li>
+            <li
+              className="nav-item pl-3"
+              onClick={handleClick('settings')}
+            >
+              <FaCog size="1.35rem" />
+            </li>
+          </ul>
+        </div>
+      </nav>
       {toggle ? (
         <div ref={clickRef} className="settings-container">
           <Settings />
